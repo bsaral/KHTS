e2def15f92c4395b01bc49e4090a17dd65c17612
bsaral <betul.saral@bil.omu.edu.tr>
2013-02-23 00:43:43 +0200

    start

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'therubyracer'
+gem 'execjs'
+gem 'twitter-bootstrap-rails'
+gem  "mysql2", ">= 0.3.11"
+gem 'bcrypt-ruby', :require => 'bcrypt'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem "better_errors"
+  gem "binding_of_caller"
+end
+
+gem 'jquery-rails'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KHTS::Application.config.secret_token = 'c1946eda828abd3b4e0d0cb1fcae29d25c7a29db125a68c0259cdccb283f2ef7dbc023f716cb66d7776866806138e6e782f7204055d07501fe4af269e4ed39b5'
