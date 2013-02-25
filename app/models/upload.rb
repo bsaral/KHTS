996e129205f6c9a75fb724a9187bca65fe3ebebe
bsaral <betul.saral@bil.omu.edu.tr>
2013-02-25 02:55:14 +0200

    null dosya hatası halledildi

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -1,6 +1,7 @@
 class Upload < ActiveRecord::Base
   attr_accessible :attach,:username,:user_id
   has_attached_file :attach
+  validates_attachment_presence :attach
   validates_attachment_size :attach, :less_than => 8.megabytes
   validates_attachment_content_type :attach, :content_type => ["application/x-gzip","application/pdf","application/vnd.oasis.opendocument.text","text/plain","image/jpeg","image/jpg","image/png","image/gif",\
 			"application/zip","application/vnd.ms-excel","application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet","application/msword",\
