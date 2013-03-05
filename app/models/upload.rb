6458fd1dd9f9e0ab795e63e6a3cf8810a099f30c
bsaral <betul.saral@bil.omu.edu.tr>
2013-03-05 15:34:43 +0200

    açıklama kısmı

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -5,7 +5,7 @@ class Upload < ActiveRecord::Base
   has_attached_file :attach
   validates_attachment_presence :attach, :message => ' Bir Dosya Seçiniz'
   validates :content, :presence => {:message => ' Açıklama alanı boş bırakılmaz' }
-  validates_attachment_size :attach, :in => 0.megabytes..8.megabytes , :message => ' Dosya Boyutunda Problem Var'
+  validates_attachment_size :attach, :in => 1...8.megabytes , :message => ' Dosya Boyutunda Problem Var'
   validates_attachment_content_type :attach, :content_type => ["application/x-gzip","application/pdf","application/vnd.oasis.opendocument.text","text/plain","image/jpeg","image/jpg","image/png","image/gif",
 			"application/zip","application/vnd.ms-excel","application/vnd.openxmlformatsofficedocument.spreadsheetml.sheet","application/msword",
 			"application/vnd.openxmlformats-officedocument.wordprocessingml.document","application/vnd.ms-powerpoint",
